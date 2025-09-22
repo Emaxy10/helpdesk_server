dae9353c7d4858929e2eb2de71a2413dafc4bb13
emaxy10 <emmanuelchigozie01@gmail.com>
2025-09-22 10:04:43 +0100

    22/9/25

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+
+class UserController extends Controller
+{
+    //
+
+      public function register(Request $request)
+    {
+        $request->validate([
+            "name"=> "required|string",
+            "email"=> "required|email|unique:users,email",
+            "password" => 'required|min:8|regex:/[A-Za-z]/|regex:/[0-9]/',  // Password validation
+        ]);
+
+        $user = User::create([
+            'name'=> $request->name,
+            'email'=> $request->email,
+            'password'=> $request->password
+        ]);
+
+        return response()->json([
+            'message' => 'Registered successfully',
+            'user' => $user,
+        ], 201);
+    }
+
+    public function login(Request $request){
+        $request->validate([
+            'email'=> 'required|email',
+            'password'=> 'required',
+        ]);
+
+        $user = User::where('email', $request->email)->first();
+
+        $token = $user->createToken('api-token')->plainTextToken;
+
+        return response()->json([
+            'message' => 'Login successful',
+            'token' => $token,
+        ]);
+    }
+}
